d2f7c2634e1dd3e95259958876e1469a50f7b0d1
Md Parvez Hossain <80127536+mdparvezhossain@users.noreply.github.com>
2021-07-02 18:17:22 +0600

    clickButtonAddStyle

---
+++ app.js
@@ -0,0 +1,10 @@
+function addStyle(){
+    const findDead = document.querySelector("#headingID");
+    findDead.classList.add("heading-style");
+
+};
+function removeStyle(){
+    const findDead = document.querySelector("#headingID");
+    findDead.classList.remove("heading-style");
+
+};
